3d093f201e39f32db3542ed0203fc91e8d2da81c
Last_GuardiaN <tonyzhao1314@gmail.com>
2019-07-29 19:24:34 +1000

    Add files via upload

---
+++ Study 2: Patterns/test/program.java
@@ -0,0 +1,9 @@
+
+public class program{
+    public static void main(String[] args) {
+    Rectangle rect = new Rectangle(10.0, 5.5);
+    
+    double w = rect.getHeight();
+    System.out.println("The width is " + w);
+    }
+}
