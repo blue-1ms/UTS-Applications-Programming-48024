7ae5c3465b56fd70b0731ba4c7cfca0e155148e3
1MSEZ <jichang.tian@gmail.com>
2019-08-29 23:15:53 +1000

    https://github.com/1MSEZ/UTS-Applications-Programming-48024.git

--- Study 2: Patterns/test/program.java
+++
@@ -1,9 +0,0 @@
-
-public class program{
-    public static void main(String[] args) {
-    Rectangle rect = new Rectangle(10.0, 5.5);
-    
-    double w = rect.getHeight();
-    System.out.println("The width is " + w);
-    }
-}
